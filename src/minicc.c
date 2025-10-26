274f8255625143ba9bb657b5800e032f5b2b3281
x0y14 <ya01ma08@gmail.com>
2025-10-26 16:50:08 +0900

    足し算と引き算の追加

--- src/minicc.c
+++ src/minicc.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
 
+    char* p = argv[1];
+
     printf(".section .text:\n");
     printf("  global _start\n");
     printf("_start:\n");
-    printf("  mov r1 %d\n", atoi(argv[1]));
+    printf("  mov r1 %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("  add r1 %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-') {
+            p++;
+            printf("  sub r1 %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        fprintf(stderr, "予期しない文字です: '%c\n", *p);
+        return 1;
+    }
+
     printf("  mov r0 0\n");
     printf("  syscall\n");
+
     return 0;
 }
